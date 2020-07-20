66ebadbf624fb6966fb904624f4fbb8d834d2f80
rtraborn <rtraborn@asu.edu>
2020-07-20 15:26:05 -0700

    Updated plot to use geom_step() instead of geom_line().

--- scripts/TSR_distribution_analysis.R
+++ scripts/TSR_distribution_analysis.R
@@ -20,8 +20,7 @@ POV12_3thresh$Pop <- "POV12"
 combined_table <- rbind(POV12_3thresh, POV84_3thresh, TEX36_3thresh)
 
 #Plotting the reverse cumulative distribution of the TSRs
-p <- ggplot(combined_table, aes(as.numeric(nTAGs), colour=Pop)) + stat_ecdf() + scale_x_log10() + scale_y_log10() 
-p + labs(y="Fraction of Total TSRs", x = "number of tags/TSR (nTAGs)")
+ggplot(combined_table, aes(as.numeric(nTAGs), colour=Pop)) + geom_step(aes(y = 1 - ..y..), stat='ecdf') + scale_x_log10() + scale_y_log10() + labs(y="Fraction of Total TSRs", x = "number of tags/TSR (nTAGs)")
 png(file="Dpulex_TSRs_nTAGs_RevCumulPlot.png")
 
 quantile.pov84 <- quantile(as.numeric(POV84_3thresh$nTAGs), probs = seq(0,1,0.1))
