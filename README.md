fec1c098f53a4ff51d27e3f01109de555b2727e6
rtraborn <rtraborn@asu.edu>
2020-07-20 09:42:01 -0700

    First commit; establishing this repo.

---
+++ README.md
@@ -0,0 +1,3 @@
+# DaphPopsTSRs
+## Exploring the distributions and properties of TSRs and TSSs from the Daphnia Populations STRIPE-seq data
+## R. Taylor Raborn - July 17, 2020
---
+++ scripts/TSR_distribution_analysis.R
@@ -0,0 +1,39 @@
+## Plotting the distribution of tags per TSR and STRIPE-seq
+
+#loading libraries
+library(ggplot2)
+
+#Let's import the datasets:
+
+POV84_3thresh <- read.table(file="TSRchitect_TSSthresh3_files/POV84_TSRset.txt",header=TRUE)
+TEX36_3thresh <- read.table(file="TSRchitect_TSSthresh3_files/TEX36_TSRset.txt",header=TRUE)
+POV12_3thresh <- read.table(file="TSRchitect_TSSthresh3_files/POV12_TSRset.txt",header=TRUE)
+
+#Combining the three datasets:
+combined_table <- rbind(POV12_3thresh, POV84_3thresh, TEX36_3thresh)
+
+#Plotting the reverse cumulative distribution of the TSRs
+p <- ggplot(combined_table, aes(as.numeric(nTAGs), colour=Pop)) + stat_ecdf() + scale_x_log10() + scale_y_log10() + labs(y="Fraction of Total TSRs", x = "number of tags/TSR (nTAGs)")
+png(file="Dpulex_TSRs_nTAGs_RevCumulPlot.png")
+
+quantile.pov84 <- quantile(as.numeric(POV84_3thresh$nTAGs), probs = seq(0,1,0.1))
+quantile.pov12 <- quantile(as.numeric(POV12_3thresh$nTAGs), probs=seq(0,1,0.1))
+quantile.tex36 <- quantile(as.numeric(TEX36_3thresh$nTAGs), probs=seq(0,1,0.1))
+
+#let's explore the quantile results for each
+quantile.pov84
+#the 90% quantile begins at 30 tags
+quantile.pov12
+#the 90% quantile begins at 57 tags 
+quantile.tex36
+#the 90% quantile begins at 52 tags
+
+POV84_top10pc <- POV84_3thresh[POV84_3thresh$nTAGs>=30,]
+POV12_top10pc <- POV12_3thresh[POV84_3thresh$nTAGs>=57,]
+TEX36_top10pc <- TEX36_3thresh[TEX36_3thresh$nTAGs>=52,]
+
+POV84_top_genes <- na.omit(POV84_top10pc$featureID)
+POV12_top_genes <- na.omit(POV12_top10pc$featureID)
+TEX36_top_genes <- na.omit(TEX36_top10pc$featureID)
+
+
